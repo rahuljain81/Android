c16012be85e9d0e6ad5a2e531b3277f8304d7fc8
rahuljain <rahuljain81@gmail.com>
2020-06-07 12:07:25 +0530

    Downloading Web Content (Async Task)

---
+++ DownloadingWebContent/app/src/main/java/com/example/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.downloadingwebcontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            Log.i("URL", urls[0]);
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current  = (char) data;
+
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed";
+            }
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        DownloadTask downloadTask =  new DownloadTask();
+        String result = null;
+        try {
+            result = downloadTask.execute("https://zappycode.com/", "https://www.google.com").get();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.i ("Result", result);
+    }
+}
