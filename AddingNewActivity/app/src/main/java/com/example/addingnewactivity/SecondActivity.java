4f933f1bc1422bdb0829e4de5346bc01be2a3e57
rahuljain <rahuljain81@gmail.com>
2020-06-13 22:49:25 +0530

    Adding a new Activity , Sharing data across activity

---
+++ AddingNewActivity/app/src/main/java/com/example/addingnewactivity/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.addingnewactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public void goback (View view) {
+        finish();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int age = intent.getIntExtra("age", 0);
+        Toast.makeText (this, Integer.toString(age), Toast.LENGTH_SHORT).show();
+    }
+}
