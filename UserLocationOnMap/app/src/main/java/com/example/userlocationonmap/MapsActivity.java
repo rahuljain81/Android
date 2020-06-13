ce9316a0fcd837f9100ba7770cec0b0f09959bcd
rahuljain <rahuljain81@gmail.com>
2020-06-13 22:31:30 +0530

    Code added for GeoLocation Name

--- UserLocationOnMap/app/src/main/java/com/example/userlocationonmap/MapsActivity.java
+++ UserLocationOnMap/app/src/main/java/com/example/userlocationonmap/MapsActivity.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -21,6 +23,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -81,6 +87,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("UserLocation"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listAddress = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    if (listAddress != null && listAddress.size() > 0) {
+                        Log.i ("PLaceInfo", listAddress.get(0).toString());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
 
