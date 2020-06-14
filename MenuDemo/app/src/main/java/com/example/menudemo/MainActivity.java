772e29c80602192d19c5c3d402a8809fefddccd3
rahuljain <rahuljain81@gmail.com>
2020-06-14 22:18:50 +0530

    Menu Demo

---
+++ MenuDemo/app/src/main/java/com/example/menudemo/MainActivity.java
@@ -0,0 +1,45 @@
+//https://samsungu.udemy.com/course/the-complete-android-oreo-developer-course/learn/lecture/8339528#questions
+package com.example.menudemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Log.i("MenuDemo", " settings");
+                return true;
+            case R.id.help:
+                Log.i("MenuDemo", " help");
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
