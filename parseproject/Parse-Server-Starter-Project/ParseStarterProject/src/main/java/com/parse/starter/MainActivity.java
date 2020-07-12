2f58a92417bcce78e173036c9030556249db40c1
rahuljain <rahuljain81@gmail.com>
2020-07-12 21:36:01 +0530

    Download Data from Parser Server

--- parseproject/Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ parseproject/Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -12,22 +12,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.SaveCallback;
+import com.parse.ParseQuery;
 
 
 public class MainActivity extends AppCompatActivity {
-
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    
+/*
+  //UPLOAD CODE
     ParseObject score = new ParseObject("Score");
-    score.put("usernamr", "rahul");
+    score.put("username", "rahul");
     score.put("score", 45);
 
     score.saveInBackground(new SaveCallback() {
@@ -37,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
           //OK
           Log.i("Success", "we saved the score");
         } else {
+          e.printStackTrace();
+        }
+      }
+    });
+*/
+
+  //DOWNLOAD CODE
+    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+    query.getInBackground("UYD7NSM7Gs", new GetCallback<ParseObject>() {
+      @Override
+      public void done(ParseObject object, ParseException e) {
+        if (e == null && object != null) {
+          Log.i ("Parse" , object.getString("username") + " " + Integer.toString(object.getInt("score")));
+        }
+        if (e != null) {
           e.printStackTrace();
         }
       }
