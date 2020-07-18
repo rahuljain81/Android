ede66108f7787fa73fccc80f4fb87b602d5249ac
rahuljain <rahuljain81@gmail.com>
2020-07-18 22:38:41 +0530

    Intial Screen
    landscape to portrait
    background Image
    Bird Movement

--- FlappyBird/core/src/com/rahuljain81/flappybird/MyGdxGame.java
+++ FlappyBird/core/src/com/rahuljain81/flappybird/MyGdxGame.java
@@ -8,26 +8,42 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
+	Texture[] birds;
+	int width, height;
+	int flapstate = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
+		birds = new Texture[2];
+		birds[0] = new Texture("bird.png");
+		birds[1] = new Texture("bird2.png");
+
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (flapstate == 0)
+			flapstate = 1;
+		else
+			flapstate = 0;
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		batch.draw(background, 0, 0, width, height);
+		batch.draw(birds[flapstate], width/2 - birds[flapstate].getWidth()/2, height/2 - birds[flapstate].getHeight()/2);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		background.dispose();
+		birds[0].dispose();
+		birds[1].dispose();
 	}
 }
