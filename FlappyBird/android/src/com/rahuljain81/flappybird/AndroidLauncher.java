15c1ebb6f94849d7e2ba6207ffe05b331eb3cd7a
rahuljain <rahuljain81@gmail.com>
2020-07-18 22:12:44 +0530

    Flappy Bird: Intial Files generated from libgdx

---
+++ FlappyBird/android/src/com/rahuljain81/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rahuljain81.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rahuljain81.flappybird.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
